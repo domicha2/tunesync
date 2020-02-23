8b1568ef85b110a036f42bdeee27ed0c210b5ea0
Jason Yuan <jasonnn.yuan@mail.utoronto.ca>
2020-02-23 18:02:23 -0500

    added username/password field to auth comp
    - only styling/html implemented
    - TODO: ts file

--- frontend/src/app/auth/auth.component.ts
+++ frontend/src/app/auth/auth.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss'],
 })
-export class AuthComponent {}
+export class AuthComponent implements OnInit {
+  ngOnInit(): void {
+    document.querySelector('title').innerText = 'Sign Up | Sign In';
+  }
+
+  /**
+   * Attempts to create an account for the user
+   */
+  onSignUp(): void {}
+
+  /**
+   * Attempts to authenticate the user
+   */
+  onSignIn(): void {}
+}
--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 import { AuthComponent } from './auth.component';
 
 @NgModule({
   declarations: [AuthComponent],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule],
 })
 export class AuthModule {}
