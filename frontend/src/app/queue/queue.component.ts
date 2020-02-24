8f14f70e8621d6b8532c03bd17c59351ccc61080
Jason Yuan <jasonnn.yuan@mail.utoronto.ca>
2020-02-24 11:07:38 -0500

    added drag and drop functionality to queue comp

--- frontend/src/app/queue/queue.component.ts
+++ frontend/src/app/queue/queue.component.ts
@@ -1,8 +1,35 @@
 import { Component } from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-queue',
   templateUrl: './queue.component.html',
   styleUrls: ['./queue.component.scss'],
 })
-export class QueueComponent {}
+export class QueueComponent {
+  // DEMO: remove hardcoded songs
+  queuedSongs = ['song A', 'Song B', 'Lose you to love me - Selena Gomez'];
+  availableSongs = ['Song d', 'my favourite song', 'Senorita'];
+
+  drop(event: CdkDragDrop<string[]>): void {
+    if (event.previousContainer === event.container) {
+      // reorder list
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+}
