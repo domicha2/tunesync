52cdb305bef8268d79821769489ef9df1cf8b964
Jason Yuan <jasonnn.yuan@mail.utoronto.ca>
2020-02-24 19:36:23 -0500

    missing commit
    - forgot to commit url in env file
    - should have referenced env.url but used env instead

--- frontend/src/app/http-wrapper.service.ts
+++ frontend/src/app/http-wrapper.service.ts
@@ -10,18 +10,18 @@ export class HttpWrapperService {
   constructor(private httpClient: HttpClient) {}
 
   post(url: string, body: any, options?: any) {
-    return this.httpClient.post(environment + url, body, options);
+    return this.httpClient.post(environment.url + url, body, options);
   }
 
   get(url: string, options?: any) {
-    return this.httpClient.get(environment + url, options);
+    return this.httpClient.get(environment.url + url, options);
   }
 
   put(url: string, body: any, options?: any) {
-    return this.httpClient.put(environment + url, body, options);
+    return this.httpClient.put(environment.url + url, body, options);
   }
 
   delete(url: string, options?: any) {
-    return this.httpClient.delete(environment + url, options);
+    return this.httpClient.delete(environment.url + url, options);
   }
 }
