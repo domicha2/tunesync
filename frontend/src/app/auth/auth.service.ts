7ff6a5e1e580cef198d3927394968611d4d52d02
Jason Yuan <jasonnn.yuan@mail.utoronto.ca>
2020-02-24 19:38:41 -0500

    added auth service

---
+++ frontend/src/app/auth/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+import { HttpWrapperService } from '../http-wrapper.service';
+
+import { Credentials } from './auth.actions';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private httpWrapperService: HttpWrapperService) {}
+
+  signIn(credentials: Credentials): Observable<any> {
+    return this.httpWrapperService.post('/auth/signin/', credentials);
+  }
+
+  signUp(credentials: Credentials): Observable<any> {
+    return this.httpWrapperService.post('/auth/signup/', credentials);
+  }
+}
