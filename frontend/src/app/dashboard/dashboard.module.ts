ff4047294c7171cf4686ade0447a6ad85b6e00a7
Jason Yuan <jasonnn.yuan@mail.utoronto.ca>
2020-02-26 18:32:35 -0500

    added messaging component

--- frontend/src/app/dashboard/dashboard.module.ts
+++ frontend/src/app/dashboard/dashboard.module.ts
@@ -3,14 +3,17 @@ import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { DashboardComponent } from './dashboard.component';
 import { RoomsComponent } from './rooms/rooms.component';
 import { UsersComponent } from './users/users.component';
+import { MessagingComponent } from './messaging/messaging.component';
+import { ControlsComponent } from './controls/controls.component';
 
 import { QueueModule } from './queue/queue.module';
-import { ControlsComponent } from './controls/controls.component';
 
 @NgModule({
   declarations: [
@@ -18,10 +21,13 @@ import { ControlsComponent } from './controls/controls.component';
     RoomsComponent,
     UsersComponent,
     ControlsComponent,
+    MessagingComponent,
   ],
   imports: [
     CommonModule,
     DragDropModule,
+    MatFormFieldModule,
+    MatInputModule,
     MatProgressBarModule,
     MatButtonModule,
     MatIconModule,
