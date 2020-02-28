b818880b61e0c6ca680f9064ee7fab4023f3a7f2
user <do.michael.7550@gmail.com>
2020-02-28 18:49:43 -0500

    update setup script

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from setuptools import setup, find_packages
-ver = '0.8.11'
+
+ver = "0.8.11"
 setup(
-    name             = 'hydra',
-    version          = ver,
-    description      = "Hydra: fossil multiplexer",
-    long_description = ("Hydra is a single sign-on gateway "
-                        "to multiple fossil repositories"),
-    author           = "Eduard Christian Dumitrescu",
-    license          = "LGPLv3",
-    url              = "https://hydra.ecd.space/eduard/hydra/",
-    packages         = find_packages(),
-    install_requires = ['django-bootstrap3', 'django-classbasedsettings',
-                        'django-simple-captcha'],
-    classifiers      = ["Programming Language :: Python :: 3 :: Only"])
+    name="tunesync",
+    version=ver,
+    description="TuneSync: play it again",
+    long_description=("Music sharing service to play music" "in sync with others"),
+    author="Michael, Jason, Roshan",
+    license="",
+    url="",
+    packages=find_packages(),
+    install_requires=[
+        "django",
+        "django-bootstrap3",
+        "django-classbasedsettings",
+        "django-simple-captcha",
+    ],
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+)
