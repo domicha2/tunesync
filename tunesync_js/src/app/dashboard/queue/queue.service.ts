cab08d1d4b4af71cd648e0b22be77abcc5a064d8
Jason Yuan <jasonnn.yuan@mail.utoronto.ca>
2020-03-08 10:06:32 -0400

    added queue service
    - gets list of queued songs

---
+++ tunesync_js/src/app/dashboard/queue/queue.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+import { HttpWrapperService } from '../../http-wrapper.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class QueueService {
+  constructor(private httpWrapperService: HttpWrapperService) {}
+
+  getQueue(): Observable<any> {
+    return this.httpWrapperService.get('/queue/');
+  }
+}
