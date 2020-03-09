3e9f53b68fed18380e09fcc2c08882a7a76a18d6
Jason Yuan <jasonnn.yuan@mail.utoronto.ca>
2020-03-09 17:56:35 -0400

    added functionality for users comp to users from store

--- frontend/src/app/dashboard/users/users.component.ts
+++ frontend/src/app/dashboard/users/users.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import {
   CdkDragDrop,
@@ -6,21 +6,41 @@ import {
   transferArrayItem,
 } from '@angular/cdk/drag-drop';
 
+import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+
 import { KickUserComponent } from './kick-user/kick-user.component';
+import { AppState } from '../../app.module';
+import { selectUsers } from '../store/dashboard.selectors';
+import * as DashboardActions from '../store/dashboard.actions';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss'],
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit, OnDestroy {
+  subscription = new Subscription();
+
   users = {
     admin: ['Jim'],
     dj: ['Alice'],
     regular: ['Bob', 'David'],
   };
 
-  constructor(private dialog: MatDialog) {}
+  constructor(private store: Store<AppState>, private dialog: MatDialog) {}
+
+  ngOnInit(): void {
+    this.store
+      .select(selectUsers)
+      .subscribe((users: DashboardActions.User[]) => {
+        console.log('users: ', users);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
   onKickUser(user): void {
     this.dialog.open(KickUserComponent, {
