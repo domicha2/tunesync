30adf00842f5ed7c76356d092c63edbc056fbd28
Jason Yuan <jasonnn.yuan@mail.utoronto.ca>
2020-03-09 14:35:55 -0400

    added auth guard to dashboard route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,13 +5,18 @@ import { AuthComponent } from './auth/auth.component';
 import { CreditsComponent } from './credits/credits.component';
 import { QueueComponent } from './dashboard/queue/queue.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: 'auth', component: AuthComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [AuthGuard],
+  },
   { path: 'credits', component: CreditsComponent },
   { path: 'queue', component: QueueComponent },
-  { path: '**', redirectTo: '/auth' },
+  { path: '**', redirectTo: 'auth' },
 ];
 
 @NgModule({
