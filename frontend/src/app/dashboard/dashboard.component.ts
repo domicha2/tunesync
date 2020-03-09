725e65714fafd564a8ac27dd250a51950ace24b3
Jason Yuan <jasonnn.yuan@mail.utoronto.ca>
2020-03-09 18:49:46 -0400

    cleaned up websocket connection (frontend)
    - moved ws code to main screen comp

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,42 +1,8 @@
 import { Component } from '@angular/core';
-import { HttpWrapperService } from '../http-wrapper.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
-  webSocket = new WebSocket('ws://localhost:8000/test/');
-
-  constructor(private httpWrapperService: HttpWrapperService) {}
-
-  ngOnInit() {
-    this.setupSocket();
-  }
-
-  setupSocket() {
-    this.webSocket.onopen = () => {
-      console.log('websocket open');
-      this.httpWrapperService
-        .post('/events/', {
-          room_id: 1,
-          author: 3,
-          parent_event_id: null,
-          args: 'asdfasdf',
-          event_type: 'M',
-        })
-        .subscribe();
-    };
-
-    this.webSocket.onmessage = message => {
-      console.log('received a message: ', message.data);
-    };
-
-    if (this.webSocket.readyState === WebSocket.OPEN) {
-      this.webSocket.onopen(null);
-    }
-
-    setTimeout(() => this.webSocket.send('hello from angular'), 1000);
-  }
-}
+export class DashboardComponent {}
--- frontend/src/app/dashboard/main-screen/main-screen.component.ts
+++ frontend/src/app/dashboard/main-screen/main-screen.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-main-screen',
   templateUrl: './main-screen.component.html',
   styleUrls: ['./main-screen.component.scss'],
 })
-export class MainScreenComponent {}
+export class MainScreenComponent implements OnInit {
+  ngOnInit(): void {
+    const webSocket = new WebSocket(environment.webSocketUrl);
+
+    webSocket.onopen = (event: Event) => {
+      console.log('connected to web socket');
+    };
+
+    webSocket.onmessage = (messageEvent: MessageEvent) => {
+      console.log('received a message', messageEvent);
+      // TODO: take action based on the data from the event
+      // TODO: could update the view or not
+    };
+
+    webSocket.onerror = (event: Event) => {
+      console.log('there was an error with the websocket');
+    };
+
+    webSocket.onclose = (closedEvent: CloseEvent) => {
+      console.log('disconnected from web socket');
+    };
+  }
+}
