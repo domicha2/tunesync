e0fca1dd07e30af5e71dd4e27182c172d5217ffd
Michael Do <do.michael.7550@gmail.com>
2020-03-10 07:48:28 -0400

    Fix websocket so that they only send by room

--- tunesync/consumers.py
+++ tunesync/consumers.py
@@ -1,23 +1,20 @@
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import JsonWebsocketConsumer
 import channels
 from tunesync.models import Event
 from asgiref.sync import async_to_sync
 
-class EventConsumer(WebsocketConsumer):
+
+class EventConsumer(JsonWebsocketConsumer):
     def connect(self):
 
-        user = self.scope["user"]
+        room = self.scope["headers"]["room_id"]
 
-        self.group_name = 'event-user-{}'.format(user)
+        self.group_name = "event-room-{}".format(room)
 
-        async_to_sync(self.channel_layer.group_add)(
-            self.group_name,
-            self.channel_name
-        )
-
+        async_to_sync(self.channel_layer.group_add)(self.group_name, self.channel_name)
 
         self.accept()
-        self.send("you are connected" + str(user) + ' ' +self.group_name)
+        self.send("you are connected " + str(user) + " " + self.group_name)
 
     def disconnect(self, close_code):
         pass
@@ -26,4 +23,4 @@ class EventConsumer(WebsocketConsumer):
         self.send(repr(text_data))
 
     def user_notify_event(self, event):
-        self.send(event['text']['event_type'])
+        self.send_json(event["text"])
