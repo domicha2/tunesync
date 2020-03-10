2dd0fd97fbc3fcb2a38c28cdd3f822012d3ac384
Michael Do <do.michael.7550@gmail.com>
2020-03-10 08:22:48 -0400

    Add blank=True for parent_event_id in model

--- tunesync/models.py
+++ tunesync/models.py
@@ -11,7 +11,6 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
-
 class Room(models.Model):
     title = models.CharField(max_length=30, unique=True)
     subtitle = models.CharField(max_length=30)
@@ -49,7 +48,9 @@ class Event(models.Model):
     creation_time = models.DateTimeField(auto_now_add=True)
     event_type = models.CharField(max_length=2, choices=EVENT_TYPE, default=MESSAGE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    parent_event_id = models.ForeignKey("self", null=True, on_delete=models.CASCADE)
+    parent_event_id = models.ForeignKey(
+        "self", null=True, blank=True, on_delete=models.CASCADE, default=None
+    )
     args = models.CharField(
         max_length=10000
     )  # this will be a serialized json in a string
@@ -100,40 +101,37 @@ class Membership(models.Model):
     PENDING = "P"
     REJECTED = "R"
     STATES = [(ACCEPTED, "Accepted"), (PENDING, "Pending"), (REJECTED, "Rejected")]
-    state = models.CharField(max_length=1, choices=STATES)
+    state = models.CharField(max_length=1, choices=STATES, default=PENDING)
     DJ = "D"
     ADMIN = "A"
     OTHER = "O"
     ROLES = [(DJ, "DJ"), (ADMIN, "Admin"), (OTHER, "Other")]
     role = models.CharField(max_length=1, choices=ROLES)
 
-@receiver(post_save, sender=Event, dispatch_uid='update_event_listeners')
+
+@receiver(post_save, sender=Event, dispatch_uid="update_event_listeners")
 def update_event_listeners(sender, instance, **kwargs):
-    '''
+    """
     Alerts consumer of new events
-    '''
+    """
     room = instance.room
     group_name = "event-room-{}".format(room)
 
     message = {
-        'room_id' : instance.room,
-        'event_id': instance.id,
-        'event_type': instance.event_type,
-        'author': instance.author,
+        "room_id": instance.room,
+        "event_id": instance.id,
+        "event_type": instance.event_type,
+        "author": instance.author,
         "parent_event_id": instance.parent_event_id,
-        'creation_time': instance.creation_time.isoformat(),
-        'args': instance.args,
+        "creation_time": instance.creation_time.isoformat(),
+        "args": instance.args,
     }
 
     channel_layer = channels.layers.get_channel_layer()
 
     async_to_sync(channel_layer.group_send)(
-        group_name,
-        {
-            'type': 'user_notify_event',
-            'text': message,
-        },
+        group_name, {"type": "user_notify_event", "text": message}
     )
 
     # need end point for websocket token
-    # signed with hmacs 
+    # signed with hmacs
