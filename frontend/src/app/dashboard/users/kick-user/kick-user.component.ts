c78b099300b396ed6464f48149e1482985609d3f
Jason Yuan <jasonnn.yuan@mail.utoronto.ca>
2020-03-10 22:54:44 -0400

    added functionality to delete user from room

--- frontend/src/app/dashboard/users/kick-user/kick-user.component.ts
+++ frontend/src/app/dashboard/users/kick-user/kick-user.component.ts
@@ -1,16 +1,28 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
+import { Store } from '@ngrx/store';
+
+import { AppState } from '../../../app.module';
+import * as DashboardActions from '../../store/dashboard.actions';
+import { User } from '../../dashboard.models';
+
 @Component({
   selector: 'app-kick-user',
   templateUrl: './kick-user.component.html',
   styleUrls: ['./kick-user.component.scss'],
 })
 export class KickUserComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public user: User,
+    private store: Store<AppState>,
+  ) {}
 
   onKick(): void {
+    this.store.dispatch(
+      DashboardActions.removeUserFromRoom({ userId: this.user.id }),
+    );
     // call backend to remove user from this room
-    console.log('kicked user' + this.data.user);
+    console.log('kicked user' + this.user.name);
   }
 }
