f36f3891cfaec5288ea087ff15135f0c6a8a87c6
Jason Yuan <jasonnn.yuan@mail.utoronto.ca>
2020-03-16 17:30:25 -0400

    removed queue route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AuthComponent } from './auth/auth.component';
 import { CreditsComponent } from './credits/credits.component';
-import { QueueComponent } from './dashboard/queue/queue.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthGuard } from './auth/auth.guard';
 
@@ -15,7 +14,6 @@ const routes: Routes = [
     canActivate: [AuthGuard],
   },
   { path: 'credits', component: CreditsComponent },
-  { path: 'queue', component: QueueComponent },
   { path: '**', redirectTo: 'auth' },
 ];
 
