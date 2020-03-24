5d7a3a7af3eec9ed61fe2f489b4d1987b75b0f13
Jason Yuan <jasonnn.yuan@mail.utoronto.ca>
2020-03-24 10:45:11 -0400

    wrapped forgotten subscription inside sub obj

--- frontend/src/app/dashboard/users/users.component.ts
+++ frontend/src/app/dashboard/users/users.component.ts
@@ -31,30 +31,32 @@ export class UsersComponent implements OnInit, OnDestroy {
   constructor(private store: Store<AppState>, private dialog: MatDialog) {}
 
   ngOnInit(): void {
-    this.store.select(selectUsers).subscribe((users: User[]) => {
-      // clear existing list of users
-      this.users = {
-        admin: [],
-        dj: [],
-        regular: [],
-      };
-      if (users) {
-        // add users to their appropriate group
-        users.forEach(user => {
-          switch (user.role) {
-            case Role.Admin:
-              this.users.admin.push(user);
-              break;
-            case Role.DJ:
-              this.users.dj.push(user);
-              break;
-            case Role.Regular:
-              this.users.regular.push(user);
-              break;
-          }
-        });
-      }
-    });
+    this.subscription.add(
+      this.store.select(selectUsers).subscribe((users: User[]) => {
+        // clear existing list of users
+        this.users = {
+          admin: [],
+          dj: [],
+          regular: [],
+        };
+        if (users) {
+          // add users to their appropriate group
+          users.forEach(user => {
+            switch (user.role) {
+              case Role.Admin:
+                this.users.admin.push(user);
+                break;
+              case Role.DJ:
+                this.users.dj.push(user);
+                break;
+              case Role.Regular:
+                this.users.regular.push(user);
+                break;
+            }
+          });
+        }
+      }),
+    );
   }
 
   ngOnDestroy(): void {
