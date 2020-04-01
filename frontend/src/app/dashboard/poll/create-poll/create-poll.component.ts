a8364089f64f73bf213c6abcf2275c55ae79ae49
Jason Yuan <jasonnn.yuan@mail.utoronto.ca>
2020-04-01 15:18:13 -0400

    retrieved all available songs into the create poll comp

--- frontend/src/app/dashboard/poll/create-poll/create-poll.component.ts
+++ frontend/src/app/dashboard/poll/create-poll/create-poll.component.ts
@@ -1,28 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { combineLatest, Observable, Subscription } from 'rxjs';
+import { debounceTime, map, startWith, tap } from 'rxjs/operators';
 import { AppState } from '../../../app.module';
-import { User, Role } from '../../dashboard.models';
-import { selectUsers } from '../../store/dashboard.selectors';
-import { tap, map } from 'rxjs/operators';
+import { Filters, Role, Song, User } from '../../dashboard.models';
+import * as DashboardActions from '../../store/dashboard.actions';
+import {
+  selectAvailableSongs,
+  selectUsers,
+} from '../../store/dashboard.selectors';
 
 @Component({
   selector: 'app-create-poll',
   templateUrl: './create-poll.component.html',
   styleUrls: ['./create-poll.component.scss'],
 })
-export class CreatePollComponent implements OnInit {
+export class CreatePollComponent implements OnInit, OnDestroy {
   testVariable = 'Hello Michael!';
+  subscription = new Subscription();
 
   regularUsers$: Observable<User[]>;
+  availableSongs$: Observable<Song[]>;
+
+  nameControl = new FormControl('');
+  albumControl = new FormControl('');
+  artistControl = new FormControl('');
 
   constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
+    this.subscription.add(
+      combineLatest([
+        this.nameControl.valueChanges.pipe(startWith('')),
+        this.albumControl.valueChanges.pipe(startWith('')),
+        this.artistControl.valueChanges.pipe(startWith('')),
+      ])
+        .pipe(
+          debounceTime(250),
+          map(([name, album, artist]) => ({
+            name,
+            album,
+            artist,
+          })),
+        )
+        .subscribe((filters: Filters) => {
+          this.store.dispatch(DashboardActions.getAvailableSongs({ filters }));
+        }),
+    );
+
+    // get a list of filtered songs
+    this.availableSongs$ = this.store
+      .select(selectAvailableSongs)
+      .pipe(tap(songs => console.log(songs)));
+
     // get a list of users in the current room
     this.regularUsers$ = this.store.select(selectUsers).pipe(
       map((users: User[]) => users.filter(user => user.role === Role.Regular)),
       tap(users => console.log('users', users)),
     );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
