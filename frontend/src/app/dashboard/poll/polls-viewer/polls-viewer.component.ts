5e106f8e9281bf32d30091b255738cc453913716
Jason Yuan <jasonnn.yuan@mail.utoronto.ca>
2020-04-08 12:48:29 -0400

    added display to polls-viewer comp

--- frontend/src/app/dashboard/poll/polls-viewer/polls-viewer.component.ts
+++ frontend/src/app/dashboard/poll/polls-viewer/polls-viewer.component.ts
@@ -1,8 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { skip } from 'rxjs/operators';
+import { AppState } from '../../../app.module';
+import { getPollsByRoom } from '../../store/dashboard.actions';
+import { selectPolls } from '../../store/dashboard.selectors';
+import { Poll, PollState } from '../poll.models';
 
 @Component({
   selector: 'app-polls-viewer',
   templateUrl: './polls-viewer.component.html',
   styleUrls: ['./polls-viewer.component.scss'],
 })
-export class PollsViewerComponent {}
+export class PollsViewerComponent implements OnInit, OnDestroy {
+  subscription = new Subscription();
+
+  pollState: PollState = {};
+  // used by ngFor instead of iterate over pollState
+  pollIds: number[] = [];
+
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit(): void {
+    this.subscription.add(
+      this.store
+        .select(selectPolls)
+        // ignore the initial value, let the api request override it
+        .pipe(skip(1))
+        .subscribe((polls: Poll[]) => {
+          // clear the poll
+          this.pollState = {};
+          this.pollIds = [];
+
+          // iterate over the polls and set the poll state
+          polls.forEach((poll: Poll) => {
+            this.pollState[poll.poll] = poll;
+            this.pollIds.push(poll.poll);
+          });
+        }),
+    );
+
+    this.store.dispatch(getPollsByRoom());
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  trackByPollId(index: number, item: number): number {
+    return item;
+  }
+}
