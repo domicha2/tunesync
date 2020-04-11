e9e3d10bc7024bd28652c7c65ef0f965a33303df
Jason Yuan <jasonnn.yuan@mail.utoronto.ca>
2020-04-11 11:27:57 -0400

    updated prod env

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   mockUrl: 'https://97e0063b-e25a-4f13-923e-35a2f1579cc1.mock.pstmn.io',
-  url: 'http://localhost:8000',
-  webSocketUrl: 'ws://localhost:8000/events/',
+  url: 'https://api.tunesync.ecd.space',
+  webSocketUrl: 'wss://api.tunesync.ecd.space/events/',
   sentryUrl: 'https://6d0eb4af612f4c62918ee814218dee0a@sentry.io/5173931',
 };
